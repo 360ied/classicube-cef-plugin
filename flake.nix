537253821f6595f7ab966c27d88f3bdc55c0c3c7
360ied <19516527+360ied@users.noreply.github.com>
2026-09-11 21:41:32 +0000

    fix(cef_binary): update cef_binary to `152.0.6+g708dc14+chromium-152.0.7977.83`

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
             "x86_64-linux" = {
               platformUrl = "linux64";
               projectArchCmake = "x86_64";
-              hash = "sha256-WAxmtyqQxHJuKDE6moOza3I1Zd59i4usoCZLO7laO+c=";
+              hash = "sha256-aMxPi3b2o+3Uxt/+6AGv7CfliXc81/1XVlS2XdlfFYU=";
             };
             # TODO test if arm builds/works before adding these
             # "aarch64-linux" = { platformUrl = "linuxarm64"; projectArchCmake = "arm64"; hash = ""; };
